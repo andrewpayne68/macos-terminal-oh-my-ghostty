a66b90ecc668039154b28b9b45a7ead8e028d937
Andrew P <40119314+andrewpayne68@users.noreply.github.com>
2026-05-16 12:48:51 +0100

    Add files via upload

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -4,13 +4,14 @@ while :
 
 clear
 
-fastfetch
+fastfetch -c archey.jsonc
 
 echo "1. Update Brew, Apps and System"
 echo "2. Install Brew Apps"
 echo "3. Midnight Commander (mc)"
 echo "4. Matrix Screensaver (CTRL-CMD-F Fullscreen)" 
 echo "5. Mole (Deep clean and optimise your Mac)"
+echo "6. Fastfetch (all)"
 echo " "
 echo "X. Exit"
 echo " "
@@ -30,6 +31,10 @@ case $choice in
     4)  cmatrix
         ;;
     5)  mo
+        ;;
+    6)  fastfetch
+        ;;
+    m)  mo
         ;;
     x)  exit
         ;;
